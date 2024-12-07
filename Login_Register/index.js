22a5c2dfdd43215f9f407c536386bafaee3973cb
Will Renkis <145504791+renkis-byte@users.noreply.github.com>
2024-12-07 14:45:22 -0800

    Add files via upload

---
+++ Login_Register/index.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+function loginPageRedirect(){
+	window.location.href = "http://localhost:8081/Group_Project/login.html";
+}
+
+function registerPageRedirect() {
+	window.location.href = "http://localhost:8081/Group_Project/register.html";
+}
